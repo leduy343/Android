ff80da71f5420ef1078801b2e897ab818c24c673
Ngocnguyen2708 <145415202+Ngocnguyen2708@users.noreply.github.com>
2025-06-18 01:45:57 +0700

    save

---
+++ app/src/main/java/com/example/tlunavigator/TaiNguyenActivity.java
@@ -0,0 +1,12 @@
+package com.example.tlunavigator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TaiNguyenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tai_nguyen);
+    }
+}
