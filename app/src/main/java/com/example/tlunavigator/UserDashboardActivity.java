bb67f0331561f0ca0a319ede5ce1a5bca11f828d
Ngocnguyen2708 <145415202+Ngocnguyen2708@users.noreply.github.com>
2025-06-20 22:59:17 +0700

    update

---
+++ app/src/main/java/com/example/tlunavigator/UserDashboardActivity.java
@@ -0,0 +1,31 @@
+package com.example.tlunavigator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserDashboardActivity extends AppCompatActivity {
+
+    private LinearLayout btnDocs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_dashboard);
+
+        // Ánh xạ nút "Tài liệu"
+        btnDocs = findViewById(R.id.btnDocs);
+
+        // Xử lý khi bấm vào "Tài liệu"
+        btnDocs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang UserAllDocumentActivity
+                Intent intent = new Intent(UserDashboardActivity.this, UserAllDocumentsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
